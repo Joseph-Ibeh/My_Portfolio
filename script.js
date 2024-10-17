f0e3410db1acdf4bb4d72b8146cab36cf13a14f0
Joseph Ibeh <josephibeh5@gmail.com>
2024-10-17 23:26:07 +0100

    i added screenshots of my project and adjusted my css accordingly

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const text = "Welcome to my portfolio";
-    let index = 0;
-  
-    function type() {
-      document.getElementById("typing-text").textContent = text.substring(0, index + 1);
-      index = (index + 1) % text.length; // Loop continuously
-      setTimeout(type, 200); // 
-    }
-  
-    type();
+  const text = "Welcome to my page";
+  let index = 0;
+
+  function type() {
+    document.getElementById("typing-text").textContent = text.substring(
+      0,
+      index + 1
+    );
+    index = (index + 1) % text.length; 
+    setTimeout(type, 200); 
+  }
+
+  type();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("nav a");
+
+  const setActiveLink = () => {
+    const currentPath = window.location.pathname;
+    navLinks.forEach((link) => {
+      if (link.getAttribute("href") === currentPath) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+  };
+
+  setActiveLink();
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      setActiveLink();
+    });
   });
-  
+});
+
+
+function toggleReadMore(projectId) {
+  const projectDetails = document.getElementById(projectId);
+  if (projectDetails.style.display === "none" || projectDetails.style.display === "") {
+    projectDetails.style.display = "block";
+  } else {
+    projectDetails.style.display = "none";
+  }
+}
